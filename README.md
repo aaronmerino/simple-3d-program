4aed2bcaf78d7dfa691b2fd2327120229d84619f
AaronJin <aaron.merino@mail.utoronto.ca>
2019-08-21 10:16:44 -0400

    control changes

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ understanding linear perspective (1pp, 2pp, 3pp)
 # WASD - FORWARDS, LEFT, BACKWARDS, RIGHT
 # X - UP
 # Z - DOWN
-# mouse - aim
+# QE - ROTATE CAMERA
+# LEFT MOUSE - AIM
 
--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-class Point {
+class Vec3 {
     constructor(x, y, z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
-    add(x, y, z) {
+    add(v) {
         
-        this.x += x;
-        this.y += y;
-        this.z += z;
+        this.x += v.x;
+        this.y += v.y;
+        this.z += v.z;
     }
 
-    dot(p) {
-        return this.x * p.x + this.y * p.y + this.z * p.z;
+    dot(v) {
+        return this.x * v.x + this.y * v.y + this.z * v.z;
     }
 
     norm2() {
@@ -26,44 +26,48 @@ class Cube {
 
         this.vertices = [];
 
-        this.vertices[0] = (new Point(0.5 , -0.5, 5));
-        this.vertices[1] = (new Point(-0.5 , -0.5, 5));
-        this.vertices[2] = (new Point(0.5 , 0.5, 5));
-        this.vertices[3] = (new Point(-0.5 , 0.5, 5));
+        this.vertices[0] = (new Vec3(0.5 , -0.5, 3));
+        this.vertices[1] = (new Vec3(-0.5 , -0.5, 3));
+        this.vertices[2] = (new Vec3(0.5 , 0.5, 3));
+        this.vertices[3] = (new Vec3(-0.5 , 0.5, 3));
         
-        this.vertices[4] = (new Point(0.5, -0.5, 4));
-        this.vertices[5] = (new Point(-0.5, -0.5, 4));
-        this.vertices[6] = (new Point(0.5, 0.5, 4));
-        this.vertices[7] = (new Point(-0.5, 0.5, 4));
+        this.vertices[4] = (new Vec3(0.5, -0.5, 2));
+        this.vertices[5] = (new Vec3(-0.5, -0.5, 2));
+        this.vertices[6] = (new Vec3(0.5, 0.5, 2));
+        this.vertices[7] = (new Vec3(-0.5, 0.5, 2));
 
         for (let v = 0; v < this.vertices.length; v++){
-            this.vertices[v].add(0, 0, 1 );
+            this.vertices[v].add(new Vec3(0, 0, 1));
         }
 
 
         this.edges = [[0, 1], [0, 2], [2, 3], [1, 3], [4,5], [4, 6], [6,7], [5, 7], [0,4], [1,5], [2,6], [3, 7]];
     }
 
-    add(x, y, z) {
-        for (let v = 0; v < this.vertices.length; v++) {
-            this.vertices[v].add(x, y, z);
+    add(v) {
+        for (let i = 0; i < this.vertices.length; i++) {
+            this.vertices[i].add(v);
         }
     }
 
     rotateAxis(axis, rad) {
         rad = rad % Math.PI;
         for (let v = 0; v < this.vertices.length; v++) {
-            this.vertices[v].x += (Math.cos(rad) + axis.x*axis.x*(1-Math.cos(rad))) * this.vertices[v].x
-            this.vertices[v].x += (axis.x*axis.y*(1 - Math.cos(rad)) - axis.z*Math.sin(rad)) * this.vertices[v].y
-            this.vertices[v].x += (axis.x*axis.z*(1 - Math.cos(rad)) + axis.y*Math.sin(rad)) * this.vertices[v].z
+            let x = this.vertices[v].x;
+            let y = this.vertices[v].y;
+            let z = this.vertices[v].z;
 
-            this.vertices[v].y += (axis.y*axis.x*(1 - Math.cos(rad)) + axis.z*Math.sin(rad)) * this.vertices[v].x
-            this.vertices[v].y += (Math.cos(rad) + axis.y*axis.y*(1-Math.cos(rad))) * this.vertices[v].y
-            this.vertices[v].y += (axis.y*axis.z*(1 - Math.cos(rad)) - axis.x*Math.sin(rad)) * this.vertices[v].z
+            this.vertices[v].x = (Math.cos(rad) + axis.x*axis.x*(1-Math.cos(rad))) * x
+            this.vertices[v].x += (axis.x*axis.y*(1 - Math.cos(rad)) - axis.z*Math.sin(rad)) * y
+            this.vertices[v].x += (axis.x*axis.z*(1 - Math.cos(rad)) + axis.y*Math.sin(rad)) * z
 
-            this.vertices[v].z += (axis.z*axis.x*(1 - Math.cos(rad)) - axis.y*Math.sin(rad)) * this.vertices[v].x
-            this.vertices[v].z += (axis.z*axis.y*(1 - Math.cos(rad)) + axis.x*Math.sin(rad)) * this.vertices[v].y
-            this.vertices[v].z += (Math.cos(rad) + axis.z*axis.z*(1-Math.cos(rad))) * this.vertices[v].z
+            this.vertices[v].y = (axis.y*axis.x*(1 - Math.cos(rad)) + axis.z*Math.sin(rad)) * x
+            this.vertices[v].y += (Math.cos(rad) + axis.y*axis.y*(1-Math.cos(rad))) * y
+            this.vertices[v].y += (axis.y*axis.z*(1 - Math.cos(rad)) - axis.x*Math.sin(rad)) * z
+
+            this.vertices[v].z = (axis.z*axis.x*(1 - Math.cos(rad)) - axis.y*Math.sin(rad)) * x
+            this.vertices[v].z += (axis.z*axis.y*(1 - Math.cos(rad)) + axis.x*Math.sin(rad)) * y
+            this.vertices[v].z += (Math.cos(rad) + axis.z*axis.z*(1-Math.cos(rad))) * z
         }
     }
 
